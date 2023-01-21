662ef1eccc7ee1246d1b032cc5e16d72f3951e6e
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-01-21 19:28:14 +0300

    Added built-in functions examples

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# python-full-course
-Python Full Course
+# Python Full Course
+
+1.  Hello, World! [[ code ]](./hello_world.py)
+2.  Main Types (str, int, bool, list, dict) [[ code ]](./main_types.py)
+3.  Built-in Functions [[ code ]](./built-in_functions.py)
---
+++ main_types.py
@@ -0,0 +1,13 @@
+#   Main Types
+
+str = 'Hello'                                               # str
+int = 10                                                    # int
+bool = True                                                 # bool
+list = [1, 2, 3, str, int, bool]                            # list
+dict = {'str': str, 'int': int, 'bool': bool}               # dict
+
+print('str =' , str)
+print('int =' , int)
+print('bool =', bool)
+print('list =', list)
+print('dict =', dict)
