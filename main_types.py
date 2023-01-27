184b4365d51f95591ce962eeb45000fa75e8f4aa
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-01-27 18:24:33 +0300

    Improived type examples

--- main_types.py
+++ main_types.py
@@ -1,19 +1,30 @@
 #   Main Types
 
-str = 'Hello'                                                   # str
-int = 10                                                        # int
-float = 10.10                                                   # float
-bool = True                                                     # bool
-list = [str, int, float, bool]                                  # list
-dict = {'str': str, 'int': int, 'float': float, 'bool': bool}   # dict
-tuple = (str, int, float, bool)                                 # tuple
-set = {str, int, float, bool}                                   # set
+str = 'Hello'                                                   # class 'str'
+int = 10                                                        # class 'int'
+float = 10.10                                                   # class 'float'
+bool = True                                                     # class 'bool'
 
-print('str =', str)
-print('int =', int)
-print('float =', float)
-print('bool =', bool)
-print('list =', list)
-print('dict =', dict)
-print('tuple =', tuple)
-print('set =', set)
+list = [str, int, float, bool]                                  # class 'list'
+dict = {'str': str, 'int': int, 'float': float, 'bool': bool}   # class 'dict'
+tuple = (str, int, float, bool)                                 # class 'tuple'
+set = {str, int, float, bool}                                   # class 'set'
+
+# class 'NoneType'
+none = None
+
+
+def fn():                                                       # class 'function'
+    pass
+
+
+print('str =', str, type(str))
+print('int =', int, type(int))
+print('float =', float, type(float))
+print('bool =', bool, type(bool))
+print('list =', list, type(list))
+print('dict =', dict, type(dict))
+print('tuple =', tuple, type(tuple))
+print('set =', set, type(set))
+print('none =', None, type(none))
+print('fn =', fn, type(fn))
